21505f6aa95ff95446aab277076f77080283c1c2
Saki-htr <saki.hattori0116@gmail.com>
2021-12-25 14:55:08 +0900

    入力した内容にidを付与し、ローカルストレージに保存する

--- memo_app/src/components/EditForm.vue
+++ memo_app/src/components/EditForm.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="form">
-    <textarea class="edit-memo"></textarea>
-    <button>更新</button>
+    <textarea class="edit-memo" v-model="tempMemo" required></textarea>
+    <button @click="addMemo">保存</button>
     <button>削除</button>
+
+    <!-- デバッグ用 -->
+    <p>tempMemo: {{ tempMemo }}</p>
   </div>
 
 </template>
 
 <script>
 export default {
-
+  data() {
+    return{
+      tempMemo: '',
+      STORAGE_KEY: 'memos-vuejs-3.0',
+      memoList: []
+    }
+  },
+  mounted() {
+    this.memoList = JSON.parse(localStorage.getItem('items') || '[]')
+  },
+  methods: {
+    saveList() {
+      localStorage.setItem('memoList', JSON.stringify(this.memoList))
+    },
+    addMemo() {
+      const value = this.tempMemo && this.tempMemo.trim()
+      if (!value) {
+        return
+      }
+      this.memoList.push({ id: Math.random().toString(), content: this.tempMemo })
+      console.log(this.memoList)
+      this.tempMemo = ''
+      this.saveList();
+    }
+  }
+  // watch: {
+  //   memoList: {
+  //     deep: true,
+  //     handler: this.saveList()
+  //   }
+  // }
 }
 </script>
 
